9c590156689ab33c3b0cc3c93925a18d9f2c12d3
Alex <alex_soares_oliveira@hotmail.com>
2024-10-04 16:00:17 -0300

    Fix: Cards

--- src/pages/Home/Habilites/habilidades.tsx
+++ src/pages/Home/Habilites/habilidades.tsx
@@ -2,18 +2,24 @@ import { styled, keyframes } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
-import HtmlIcon from '@mui/icons-material/Html';
-import CssIcon from '@mui/icons-material/Css';
-import JavascriptIcon from '@mui/icons-material/Javascript';
-import CodeIcon from '@mui/icons-material/Code';
-import ViewQuiltIcon from '@mui/icons-material/ViewQuilt';
-import SettingsEthernetIcon from '@mui/icons-material/SettingsEthernet';
-import PaletteIcon from '@mui/icons-material/Palette';
-import GradientIcon from '@mui/icons-material/Gradient';
-import BuildIcon from '@mui/icons-material/Build';
-import BuildCircleIcon from '@mui/icons-material/BuildCircle';
-import BugReportIcon from '@mui/icons-material/BugReport';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 
+import HTML from '../../../assets/icons/html-1.svg';
+import Css from '../../../assets/icons/css-3.svg';
+import JavaScript from '../../../assets/icons/javascript-1.svg';
+import TypeScript from '../../../assets/icons/typescript.svg';
+import Jquery from '../../../assets/icons/jquery.svg';
+import Bootstrap from '../../../assets/icons/bootstrap-5-1.svg';
+import Redux from '../../../assets/icons/redux.svg';
+import Vue from '../../../assets/icons/vue-9.svg';
+import React from '../../../assets/icons/react-2.svg';
+import Sass from '../../../assets/icons/sass-1.svg';
+import Less from '../../../assets/icons/less.svg';
+import Gulp from '../../../assets/icons/gulp.svg';
+import Grunt from '../../../assets/icons/grunt-1.svg';
+import Material from '../../../assets/icons/material-ui-1.svg';
+import Cypress from '../../../assets/icons/cypress-1.svg';
 
 const fadeInScale = keyframes`
   0% {
@@ -26,28 +32,28 @@ const fadeInScale = keyframes`
   }
 `;
 
-const StyledHabilidades = styled("div")(({ theme }) => ({
-  textAlign: "center",
-  alignItems: "center",
-  minHeight: "100vh",
-  display: "flex",
-  justifyContent: "center",
-  flexDirection: "column",
-  paddingTop: "20px",
-  paddingBottom: "16px",
+const StyledHabilidades = styled('div')(({ theme }) => ({
+  textAlign: 'center',
+  alignItems: 'center',
+  minHeight: '100vh',
+  display: 'flex',
+  justifyContent: 'center',
+  flexDirection: 'column',
+  paddingTop: '20px',
+  paddingBottom: '16px',
   backgroundColor: theme.palette.grey[300],
 }));
 
-const IconContainer = styled('div')(({ color }) => ({
+const IconContainer = styled('div')(({ theme }) => ({
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
-  margin: '20px',
-  padding: '20px',
+  margin: '10px',
+  padding: '10px',
   borderRadius: '50%',
-  backgroundColor: color,
-  width: '80px',
-  height: '80px',
+  backgroundColor: theme.palette.primary.light,
+  width: '60px',
+  height: '60px',
   transition: 'transform 0.3s',
   boxShadow: '0px 6px 12px rgba(0, 0, 0, 0.1)',
   animation: `${fadeInScale} 0.8s ease-in-out`,
@@ -56,50 +62,68 @@ const IconContainer = styled('div')(({ color }) => ({
     transform: 'scale(1.2)',
     boxShadow: '0px 12px 24px rgba(0, 0, 0, 0.2)',
   },
+
+  '& img': {
+    width: '100%',
+    height: '100%',
+    objectFit: 'contain',
+  },
 }));
 
-const icons = [
-  { Icon: HtmlIcon, name: 'HTML', color: '#E34F26' },
-  { Icon: CssIcon, name: 'CSS', color: '#1572B6' },
-  { Icon: JavascriptIcon, name: 'JavaScript', color: '#F7DF1E' },
-  { Icon: CodeIcon, name: 'React', color: '#61DAFB' },
-  { Icon: BuildIcon, name: 'Material-Ui', color: '#8ddb44' },
-  { Icon: PaletteIcon, name: 'Sass', color: '#CC6699' },
-  { Icon: CodeIcon, name: 'TypeScript', color: '#3178C6' },
-  { Icon: CodeIcon, name: 'Vue.js', color: '#42b883' },
-  { Icon: ViewQuiltIcon, name: 'Bootstrap', color: '#563d7c' },
-  { Icon: SettingsEthernetIcon, name: 'jQuery', color: '#0769AD' },
-  { Icon: GradientIcon, name: 'Less', color: '#1D365D' },
-  { Icon: BuildIcon, name: 'Gulp', color: '#DB4446' },
-  { Icon: BuildCircleIcon, name: 'Grunt', color: '#FBA919' },
-  { Icon: BugReportIcon, name: 'Cypress', color: '#00B6B4' },
-  { Icon: BugReportIcon, name: 'Testes Unitarios', color: '#00B6B4' },
-];
+const StyledCard = styled(Card)(({ }) => ({
+  maxWidth: 600,
+  margin: '20px auto',
+  padding: '20px',
+  textAlign: 'center',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
+  '&:hover': {
+    boxShadow: '0px 12px 24px rgba(0, 0, 0, 0.2)',
+  },
+}));
 
 export default function Habilidades() {
+  const icons = [
+    HTML,
+    Css,
+    JavaScript,
+    TypeScript,
+    Jquery,
+    Bootstrap,
+    Redux,
+    Vue,
+    React,
+    Sass,
+    Less,
+    Gulp,
+    Grunt,
+    Material,
+    Cypress,
+  ];
+
   return (
     <StyledHabilidades id="Habilidades">
       <Container maxWidth="lg">
-        <Typography
-          variant="h2"
-          textAlign="center"
-          gutterBottom
-        >
+        <Typography variant="h2" textAlign="center" gutterBottom>
           Habilidades
         </Typography>
 
-        <Grid container spacing={4} justifyContent="center">
-          {icons.map((stack, index) => (
-            <Grid item xs={6} sm={4} md={2} key={index}>
-              <IconContainer color={stack.color}>
-                <stack.Icon sx={{ fontSize: '2rem', color: '#fff' }} />
-              </IconContainer>
-              <Typography variant="h6" textAlign="center" color="textPrimary">
-                {stack.name}
-              </Typography>
+        <StyledCard>
+          <CardContent>
+            <Grid container spacing={2} justifyContent="center">
+              {icons.map((icon, index) => (
+                <Grid item key={index}>
+                  <IconContainer>
+                    <img src={icon} alt="" />
+                  </IconContainer>
+                </Grid>
+              ))}
             </Grid>
-          ))}
-        </Grid>
+          </CardContent>
+        </StyledCard>
       </Container>
     </StyledHabilidades>
   );
